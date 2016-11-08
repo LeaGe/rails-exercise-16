416acd997ffe0987301a32ee63c771fa9442afa6
LeaGe <lea.gerneth@student.hpi.de>
2016-11-08 14:59:40 +0100

    Failing test for #8

--- spec/features/authors_index_spec.rb
+++ spec/features/authors_index_spec.rb
@@ -5,4 +5,10 @@ describe "Author index page", :type => :feature do
   it "should render" do
     visit authors_path
   end
+
+  it "should list name and hompage of all authors" do
+    create(:author)
+    visit authors_path
+    expect(page).to have_text("alan turing")
+  end
 end
