665a0ecf49c5ef38d17d953ebd5f0f67f47d05bd
LeaGe <lea.gerneth@student.hpi.de>
2016-11-08 15:17:03 +0100

    Failing test for #9

--- spec/features/authors_index_spec.rb
+++ spec/features/authors_index_spec.rb
@@ -11,4 +11,9 @@ describe "Author index page", :type => :feature do
     visit authors_path
     expect(page).to have_text("Alan Turing")
   end
+
+  it "should link to the new author page" do
+    visit authors_path
+    expect(page).to have_css("a", text:"add author")
+  end
 end
