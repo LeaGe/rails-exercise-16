6a4fda61f27f2f2e5d76a7744cdc7b826fbcb07e
LeaGe <lea.gerneth@student.hpi.de>
2016-11-08 14:12:15 +0100

    Failing test for #6

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -7,8 +7,17 @@ describe "Authors page", :type => :feature do
     expect(page).to have_text("New Author")
   end
 
-  it 'should have input fields' do
+  it 'should save the author' do
     visit new_author_path
-    expect(page).to have_field("authors_first_name")
+    fill_in "author_first_name", with: "Alan"
+    fill_in "author_last_name", with: "Turing"
+    fill_in "author_homepage", with: "'http://wikipedia.org/Alan_Turing"
+    click_button "Save Authors"
+    expect(Author.find_by(first_name: "Alan", last_name: "Turing"))
   end
+  #it 'should have input fields' do
+    #visit new_author_path
+    #expect(page).to have_field("authors_first_name")
+  #end
+
 end
