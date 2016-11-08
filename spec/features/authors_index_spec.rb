eecabe0790e049530b3160a49a95eb526f92a154
LeaGe <lea.gerneth@student.hpi.de>
2016-11-08 15:34:59 +0100

    Failing Test for #11

--- spec/features/authors_index_spec.rb
+++ spec/features/authors_index_spec.rb
@@ -16,4 +16,10 @@ describe "Author index page", :type => :feature do
     visit authors_path
     expect(page).to have_css("a", text:"Add author")
   end
+
+  it "should link to author page" do
+    create(:author)
+    visit authors_path
+    expect(page).to have_css("a", text:"Show")
+  end
 end
