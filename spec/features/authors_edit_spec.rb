46fc2bc20bfef414bf33ed66376da4f253c33288
LeaGe <lea.gerneth@student.hpi.de>
2016-11-09 07:02:53 +0100

    Failing test for #15

--- spec/features/authors_edit_spec.rb
+++ spec/features/authors_edit_spec.rb
@@ -7,4 +7,12 @@ describe "Edit Author page", :type => :feature do
     visit edit_author_path(author)
   end
 
+  it "should render" do
+    author = create(:author)
+    visit edit_author_path(author)
+    fill_in "author_last_name", with: "Mathison"
+    click_button "Submit"
+    expect(Author.find_by(first_name: "Alan").last_name). to eql("Mathison")
+  end
+
 end
