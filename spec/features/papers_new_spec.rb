95645e145b0a088fac5ebbcef2ebbed116aea7f7
LeaGe <lea.gerneth@student.hpi.de>
2016-11-09 13:21:34 +0100

    Failing Test for #20

--- spec/features/papers_new_spec.rb
+++ spec/features/papers_new_spec.rb
@@ -5,4 +5,10 @@ describe "New paper page", :type => :feature do
   it "should render" do
     visit new_paper_path
   end
+
+  it 'should have input fields' do
+    visit new_paper_path
+    expect(page).to have_field("Titel")
+  end
+
 end
