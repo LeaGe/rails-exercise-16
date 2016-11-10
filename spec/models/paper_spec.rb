02f3afc2cfd57b1428b54aa8bbdd88822c56152f
LeaGe <lea.gerneth@student.hpi.de>
2016-11-10 17:20:54 +0100

    Failing Test for #29

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
 
+  describe "Paper" do
+    it "should not validate without title" do
+      expect(build(:paper, title:nil, venue: "mind 49: 433-460", year: 1950)).to_not be_valid
+    end
+  end
+
 end
