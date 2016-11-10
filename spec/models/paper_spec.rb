8387334e27df667b45bebfb06a196975d42f1a6f
LeaGe <lea.gerneth@student.hpi.de>
2016-11-10 17:51:18 +0100

    Failing test for #32

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -8,8 +8,12 @@ RSpec.describe Paper, type: :model do
       expect(build(:paper, title:nil, venue: "mind 49: 433-460", year: 1950)).to_not be_valid
     end
 
-    it "should not validate without title" do
+    it "should not validate without venue" do
       expect(build(:paper, title: "computing machinery and intelligence", venue: nil, year: 1950)).to_not be_valid
+    end
+
+    it "should not validate with non-integer year" do
+      expect(build(:paper, title: "computing machinery and intelligence", venue: "mind 49: 433-460", year: 'nineteen-fifty')).to_not be_valid
     end
 
   end
