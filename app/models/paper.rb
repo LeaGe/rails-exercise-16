7975a644fd41c7123c519b84af634149a5df8a68
LeaGe <lea.gerneth@student.hpi.de>
2016-11-10 17:28:18 +0100

    Failing test for #30

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,3 +1,4 @@
 class Paper < ActiveRecord::Base
   validates :title, presence: true
+  validates :title, presence: true
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe Paper, type: :model do
 
   describe "Paper" do
+
     it "should not validate without title" do
       expect(build(:paper, title:nil, venue: "mind 49: 433-460", year: 1950)).to_not be_valid
     end
+
+    it "should not validate without title" do
+      expect(build(:paper, title: "computing machinery and intelligence", venue: nil, year: 1950)).to_not be_valid
+    end
+
   end
 
 end
