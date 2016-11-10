2c6565faa06f15dee83abb2c782f58331c3a89e4
LeaGe <lea.gerneth@student.hpi.de>
2016-11-10 18:12:00 +0100

    Closes #33

---
+++ spec/features/paper_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "Paper page", :type => :feature do
+
+  it "should display paper details" do
+    paper = create(:paper)
+    visit paper_path(paper)
+    expect(page).to have_text("COMPUTING MACHINERY AND INTELLIGENCE")
+  end
+end
