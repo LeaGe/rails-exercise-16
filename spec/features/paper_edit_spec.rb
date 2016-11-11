533755e067a7a8ac6c5cf20996f19f35f93aaca1
LeaGe <lea.gerneth@student.hpi.de>
2016-11-11 12:22:35 +0100

    erstes Dropdown. Closes #45

--- spec/features/paper_edit_spec.rb
+++ spec/features/paper_edit_spec.rb
@@ -10,4 +10,10 @@ describe "Edit Paper page", :type => :feature do
     expect(Paper.find_by(year: 1950).venue). to eql("Mind 49")
   end
 
+  it "should allow to select 5 authors from 5 separate drop downs" do
+    paper = create(:paper)
+    visit edit_paper_path(paper)
+    expect(page).to have_field("Author 1")
+  end
+
 end
